33dcf71514f1aba757e0a260a5e76641007dcf7e
Rockey Ahmed <rockeycse@gmail.com>
2021-06-26 09:38:28 +0600

    file data chunk

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -15,18 +15,21 @@ class SalesController extends Controller
     {
         if (request()->has('mycsv')) {
 
-            $data = array_map('str_getcsv', file(request()->mycsv));
+            $data = file(request()->mycsv);
             $header = $data[0];
-            $chunks = array_chunk($data, 1000);
+            $chunks = array_chunk($data, 100);
 
             foreach ($chunks as $key => $chunk) {
-                $data = array_map('str_getcsv', $chunk);
-                $name = '';
-                file_put_contents();
-                if ($key === 0) {
-                    $header = $data[0];
-                    unset($data[0]);
-                }
+                // $data = array_map('str_getcsv', $chunk);
+                $name = "/temp{$key}.CSV";
+                $path = resource_path('temp');
+                file_put_contents($path . $name, $chunk);
+
+                // return $path . $name;
+                // if ($key === 0) {
+                //     $header = $data[0];
+                //     unset($data[0]);
+                // }
             }
 
 
