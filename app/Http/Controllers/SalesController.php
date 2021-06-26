e4ea3ec8bd89faa92959a0adb6f0e723d5bed540
Rockey Ahmed <rockeycse@gmail.com>
2021-06-26 22:48:24 +0600

    validation of file

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -17,32 +17,40 @@ class SalesController extends Controller
     public function upload()
     {
         if (request()->has('mycsv')) {
-            $data   =   file(request()->mycsv);
-            // Chunking file
-            $chunks = array_chunk($data, 300);
+            $ext = request()->file('mycsv')->getClientOriginalExtension();
+            if ($ext == 'CSV' or $ext == 'csv') {
 
-            $header = [];
-            $batch  = Bus::batch([])->dispatch();
-            
-            $i = 0;
-            foreach ($chunks as $key => $chunk) {
-                $data = array_map('str_getcsv', $chunk);
 
-                if ($key === 0) {
-                    $header = $data[0];
-                    unset($data[0]);
+                $data   =   file(request()->mycsv);
+                // Chunking file
+                $chunks = array_chunk($data, 300);
+
+                $header = [];
+                $batch  = Bus::batch([])->dispatch();
+
+                $i = 0;
+                foreach ($chunks as $key => $chunk) {
+                    $data = array_map('str_getcsv', $chunk);
+
+                    if ($key === 0) {
+                        $header = $data[0];
+                        unset($data[0]);
+                    }
+
+                    // SalesCsvProcess::dispatch($data, $header);
+                    $batch->add(new SalesCsvProcess($data, $header));
+                    if ($i == 10) {
+                        return "Data Stored";
+                    } else {
+                        $i++;
+                    }
                 }
 
-                // SalesCsvProcess::dispatch($data, $header);
-                $batch->add(new SalesCsvProcess($data, $header));
-                if ($i == 10) {
-                    return "Data Stored";
-                } else {
-                    $i++;
-                }
+
+                return $batch;
+            }else{
+                return "Please Upload CSV file only";
             }
-
-            return $batch;
         }
 
         return 'please upload file';
