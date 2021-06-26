076a91e4c52cd94019ddffbc6208e85ae75259ad
Rockey Ahmed <rockeycse@gmail.com>
2021-06-26 09:46:46 +0600

    created jobs

---
+++ app/Jobs/SalesCsvProcess.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Sales;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class SalesCsvProcess implements ShouldQueue
+{
+    use  Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $data;
+    public $header;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($data, $header)
+    {
+        $this->data   = $data;
+        $this->header = $header;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        foreach ($this->data as $sale) {
+            $saleData = array_combine($this->header, $sale);
+            Sales::create($saleData);
+        }
+    }
+
+    public function failed(Throwable $exception)
+    {
+        // Send user notification of failure, etc...
+    }
+}
